9ccae822dad0a638e935c2c0223b277996fa12e4
krishna-paulraj <krishnapaulraj2004@gmail.com>
2026-05-30 23:28:18 +0530

    feat: add GitHub contributions graph component

---
+++ lib/get-cached-contributions.ts
@@ -0,0 +1,20 @@
+import { unstable_cache } from "next/cache"
+
+import type { Activity } from "@/components/contribution-graph"
+
+type GitHubContributionsResponse = {
+  contributions: Activity[]
+}
+
+export const getCachedContributions = (username: string) =>
+  unstable_cache(
+    async () => {
+      const res = await fetch(
+        `${process.env.GITHUB_CONTRIBUTIONS_API_URL || `https://github-contributions-api.jogruber.de`}/v4/${username}?y=last`
+      )
+      const data = (await res.json()) as GitHubContributionsResponse
+      return data.contributions
+    },
+    [`github-contributions-${username}`],
+    { revalidate: 86400 }
+  )()
